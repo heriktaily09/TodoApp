c8eb9c253079f9b4ada0561fad91ea516db2cc6e
Herik Taily <heriktaily09@gmail.com>
2024-06-29 10:25:26 +0530

    push project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
+import TodoForm from "./TodoForm";
+import Todos from "./Todos";
+import { useState } from "react";
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
+  let [todos, setTodos] = useState([
+    { id: 1, title: "Learn Guitar", completed: false },
+    { id: 2, title: "Learn React", completed: false },
+    { id: 3, title: "Find Good waltz in Am", completed: false }
+  ]);
+  const addTodo = (newTodo) => {
+    setTodos(prevState => [...prevState, newTodo]);
+  }
+  const toggleCompleted = (id) => {
+    // const newTodos = [];
+    // for (let todo of todos) {
+    //   if (todo.id === id) {
+    //     newTodos.push({ ...todo, completed: !todo.completed })
+    //   } else {
+    //     newTodos.push(todo);
+    //   }
+    // }
+    // setTodos(newTodos);
+
+    setTodos(prevState => {
+      return prevState.map(todo => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed }
+        } else {
+          return todo;
+        }
+      })
+
+    });
+  }
+  const removeTodo = (id) => {
+    setTodos(prevState => prevState.filter(todo => id !== todo.id));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div class="container">
+      <ToastContainer />
+      <h1 className="main-title">Todo List</h1>
+      <TodoForm addTodo={addTodo} />
+      <Todos todos={todos} toggleCompleted={toggleCompleted} removeTodo={removeTodo} />
     </div>
   );
 }
---
+++ src/TodoForm.jsx
@@ -0,0 +1,38 @@
+import React from "react";
+import { useState } from "react";
+import { v4 as uuid } from "uuid";
+import { toast } from "react-toastify";
+function TodoForm({ addTodo }) {
+  let [title, setTitle] = useState("");
+  function handleSubmit(e) {
+    e.preventDefault();
+    if (title.trim().length === 0) {
+      toast.error("Please fill input!", {
+        autoClose: 1750,
+      });
+      return;
+    }
+    const newTodo = {
+      title: title,
+      completed: false,
+      id: uuid(),
+    };
+    addTodo(newTodo);
+    setTitle("");
+  }
+  return (
+    <form onSubmit={handleSubmit} className="todoForm">
+      <input
+        className="todoForm__input"
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <button type="submit" className="todoForm__btn">
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
